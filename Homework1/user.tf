aa3d9924e28db504c370f03fea750afdb5d28757
nazikmusa <musaevan331@gmail.com>
2025-01-18 20:35:00 +0000

    group membership added

--- Homework1/user.tf
+++ Homework1/user.tf
@@ -37,3 +37,29 @@ resource "aws_iam_user" "dahyun" {
 resource "aws_iam_group" "twice" {
   name = "twice"
 }
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.jenny.name,
+    aws_iam_user.rose.name,
+    aws_iam_user.lisa.name,
+    aws_iam_user.jisoo.name,
+  ]
+
+  group = aws_iam_group.blackpink.name
+}
+
+resource "aws_iam_group_membership" "team1" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.jihyo.name,
+    aws_iam_user.sana.name,
+    aws_iam_user.momo.name,
+    aws_iam_user.dahyun.name,
+  ]
+
+  group = aws_iam_group.twice.name
+}
