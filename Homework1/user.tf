48700493b27b46efdce06321fa2d4d9ed960fa40
nazikmusa <musaevan331@gmail.com>
2025-01-18 20:12:47 +0000

    users, groups added

--- Homework1/user.tf
+++ Homework1/user.tf
@@ -1,3 +1,39 @@
-resource "aws_iam_user" "mona" {
-  name = "mona"
+resource "aws_iam_user" "jenny" {
+  name = "jenny"
+}
+
+resource "aws_iam_user" "rose" {
+  name = "rose"
+}
+
+resource "aws_iam_user" "lisa" {
+  name = "lisa"
+}
+
+resource "aws_iam_user" "jisoo" {
+  name = "jisoo"
+}
+
+resource "aws_iam_group" "blackpink" {
+  name = "blackpink"
+}
+
+resource "aws_iam_user" "jihyo" {
+  name = "jihyo"
+}
+
+resource "aws_iam_user" "sana" {
+  name = "sana"
+}
+
+resource "aws_iam_user" "momo" {
+  name = "momo"
+}
+
+resource "aws_iam_user" "dahyun" {
+  name = "dahyun"
+}
+
+resource "aws_iam_group" "twice" {
+  name = "twice"
 }
