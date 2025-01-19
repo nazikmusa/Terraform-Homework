c1145a269891f6e5ed418bfa592532862ca8ad7d
nazikmusa <musaevan331@gmail.com>
2025-01-19 03:28:15 +0000

    users added to group

---
+++ Homework2/users.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_user" "lb" {
+  for_each = toset(["jihyo", "sana", "momo", "dahyun"])
+  name = each.value
+}
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    for i in aws_iam_user.lb : i.name 
+  ]
+
+  group = aws_iam_group.group.name
+}
+
+resource "aws_iam_group" "group" {
+  name = "twice"
+}
+
+
