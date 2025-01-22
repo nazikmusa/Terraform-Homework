8622131c332c1d9f881856893d81e102fe3c717a
nazikmusa <musaevan331@gmail.com>
2025-01-22 00:38:17 +0000

    apache updated

--- Homework3/apache.sh
+++ Homework3/apache.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd 
-sudo echo "Hello World" >> /var/www/html/index.html
+sudo apt update
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo echo Hello World >> /var/www/html/index.html
 
--- Homework3/main.tf
+++ Homework3/main.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250115"]
   }
 
   filter {
@@ -40,7 +40,7 @@ resource "aws_instance" "web1" {
 }
 
 resource "aws_instance" "web2" {
-  ami           = "ami-0cdd6d7420844683b"
+  ami           = "ami-07eef52105e8a2059"
   instance_type = "t2.micro"
   availability_zone = "eu-central-1b"
   subnet_id = "subnet-0ffe1ebdd6ad3101e"
