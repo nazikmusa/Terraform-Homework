d6909bf7409ac7eec5bce3ce00e6dcb030ecc2b4
nazikmusa <musaevan331@gmail.com>
2025-01-26 02:21:18 +0000

    intance created

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -1,6 +1,12 @@
 provider aws {
     region = var.region
 }
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.aws_key_pair
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
@@ -15,6 +21,7 @@ resource "aws_subnet" "main" {
     Name = "Main"
   }
 }
+
 
 resource "aws_subnet" "main2" {
   vpc_id     = aws_vpc.main.id
@@ -38,45 +45,17 @@ resource "aws_subnet" "main3" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "example"
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.example.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.main2.id
-  route_table_id = aws_route_table.example.id
-}
-
-resource "aws_route_table_association" "c" {
-  subnet_id      = aws_subnet.main3.id
-  route_table_id = aws_route_table.example.id
-}
 
 
 
-# output vpc {
-#   value = aws_vpc.main.id
-#   sensitive = false
-# }
+
+
+
+
+
+
+
+
+
+
+
--- Homework4/variable.tf
+++ Homework4/variable.tf
@@ -1,25 +1,35 @@
 variable region {
     default = "us-east-2"
-    type = string
-    description = "Provide a region"
 }
-variable vpc_cidr {
-    default = "10.0.0.0/16"
-    type = string
-}
-variable subnet1 {
-  default = "10.0.1.0/24"
-}
-variable subnet2 {
-  default = "10.0.2.0/24"
-}
-variable subnet3 {
-  default = "10.0.3.0/24"
-}
-variable instance_type {
-  default = "t2.micro"
+
+variable aws_key_pair {
+  default = "deployer"
 }
 variable port {
   type = list
   default = [22, 80, 443]
+}
+variable vpc_cidr {
+  default = "10.0.0.0/16"
+  type = string
+}
+variable subnet1 {
+    default = "10.0.1.0/24"
+}
+variable subnet2 {
+    default = "10.0.2.0/24"
+}
+variable subnet3 {
+    default = "10.0.3.0/24"
+}
+variable instance_type {
+  default = "t2.micro"
+}
+
+variable ami {
+  default = "ami-0eb070c40e6a142a3"
+}
+
+variable instance_name {
+  default = "HelloWorld"
 }
